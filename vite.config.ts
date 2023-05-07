9c8ead62fef1cf754934ed6362fd25ea68fcd967
Artyom Chernetsov <chernetsov.artyom@gmail.com>
2023-05-07 17:51:12 +0300

    Gitlab pages fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // fixes for gitabl pages: https://forum.gitlab.com/t/no-access-control-allow-origin-header-for-https-projects-gitlab-io-auth/54904
+  base: '/flowbite-vue-router-tabs',
   plugins: [vue()],
   resolve: {
     alias: {
